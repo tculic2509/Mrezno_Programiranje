525faa37cb270beb015f3bd7256700f33ba6e295
Toni Čulić <125596981+tculic2509@users.noreply.github.com>
2023-06-02 12:38:11 +0200

    HTTP.py

--- vj6/HTTP.py
+++ vj6/HTTP.py
@@ -1,18 +1,13 @@
 import http.server
 import socketserver
 
-
-# Definirajte klasu koja će obrađivati zahtjeve klijenata
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Postavite HTTP status kod 200 (OK)
         self.send_response(200)
 
-        # Postavite zaglavlje Content-type
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        # Definirajte sadržaj web stranice u HTML formatu
         content = """
         <html>
         <head>
@@ -26,20 +21,12 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         </body>
         </html>
         """
-
-        # Pošaljite sadržaj kao odgovor klijentu
         self.wfile.write(content.encode())
 
-
-# Postavite server na željeni port (u ovom slučaju 8000)
 port = 8000
-# Slušajte na svim dostupnim sučeljima (0.0.0.0)
 server_address = ('', port)
-
-# Stvorite instancu HTTPServer klase s definiranim request handlerom
 httpd = socketserver.TCPServer(server_address, MyHttpRequestHandler)
 
-# Pokrenite server
 print(f"Server sluša na portu {port}.")
 httpd.serve_forever()
 
