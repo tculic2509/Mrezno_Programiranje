87b8e68677101bfc115efb80f89f45de3459f58d
Toni Čulić <125596981+tculic2509@users.noreply.github.com>
2023-06-02 12:38:59 +0200

    DNS.py

--- vj6/DNS.py
+++ vj6/DNS.py
@@ -1,12 +1,8 @@
 import dns.resolver
 
-
 def dns_lookup(query, record_type):
     try:
-        # Izvršite DNS upit za određeni tip zapisa
         answers = dns.resolver.resolve(query, record_type)
-
-        # Prikupite rezultate upita
         results = []
         for answer in answers:
             results.append(str(answer))
@@ -21,18 +17,11 @@ def dns_lookup(query, record_type):
 
     except dns.exception.Timeout:
         return "Nema rezultata. DNS upit je istekao."
-
-
-# Unesite DNS ime ili IP adresu
 query = input("Unesite DNS ime ili IP adresu: ")
 
-# Unesite tip DNS zapisa (A, MX ili PTR)
 record_type = input("Unesite tip DNS zapisa (A, MX ili PTR): ")
 
-# Izvršite DNS upit i dohvatite rezultate
 results = dns_lookup(query, record_type)
-
-# Ispis rezultata
 print("Rezultati DNS upita:")
 for result in results:
     print(result)
